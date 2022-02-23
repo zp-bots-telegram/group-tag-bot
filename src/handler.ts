5b79b55dfd7f462ef3e4a8a1cd576cf3f59b2d1f
Zack Pollard <zackpollard@ymail.com>
2022-02-23 11:36:43 +0000

    feat: base setup for telegram bot

---
+++ src/handler.ts
@@ -0,0 +1,37 @@
+import express, { Request, Response } from 'express';
+
+import { Telegraf } from 'telegraf';
+import { getEnv } from 'shared/env';
+import { devSetup } from 'devSetup';
+
+export async function handler() {
+  const token = process.env.BOT_TOKEN;
+  if (!token) {
+    throw new Error('BOT_TOKEN must be provided!');
+  }
+
+  if (getEnv('NODE_ENV') === 'development') {
+    await devSetup();
+  }
+
+  const bot = new Telegraf(token);
+
+  const host = getEnv('host');
+  const secretPath = `/telegraf/${bot.secretPathComponent()}`;
+
+  await bot.telegram.setWebhook(`${host}${secretPath}`);
+
+  const app = express();
+  app.get('/', (req: Request, res: Response) => res.send('Hello World!'));
+  // Set the bot API endpoint
+  app.use(bot.webhookCallback(secretPath));
+  app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+  });
+}
+
+handler()
+  .then(() => console.log('Bot Running'))
+  .catch((error) => {
+    console.error('Uncaught Error Thrown', error);
+  });
