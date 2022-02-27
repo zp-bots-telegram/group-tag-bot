eef34248f3293ba4e15696be270a24816024b5ef
Zack Pollard <zackpollard@ymail.com>
2022-02-27 00:25:24 +0000

    feat: tag delete command

--- src/command/commands.ts
+++ src/command/commands.ts
@@ -2,11 +2,13 @@ import { Telegraf } from 'telegraf';
 import { createCommand } from 'command/createCommand';
 import { tagsCommand } from 'command/tagsCommand';
 import { addCommand } from 'command/addCommand';
+import { deleteCommand } from 'command/deleteCommand';
 import { infoCommand } from 'command/infoCommand';
 
 export function registerCommands(bot: Telegraf) {
   addCommand(bot);
   createCommand(bot);
   tagsCommand(bot);
+  deleteCommand(bot);
   infoCommand(bot);
 }
